4ae547f3c351ae49686b2d544f782fc904399e00
douzone-busan-bitacademy <douzone.busan.bitacademy@gmail.com>
2021-05-27 11:49:56 +0900

    user #1 + serverside include

--- mysite02/src/main/java/com/douzone/mysite/mvc/util/MvcUtils.java
+++ mysite02/src/main/java/com/douzone/mysite/mvc/util/MvcUtils.java
@@ -19,4 +19,12 @@ public class MvcUtils {
 		rd.forward(request, response);
 	}
 
+	public static void redirect(
+		String url,
+		HttpServletRequest request,
+		HttpServletResponse response)
+		throws ServletException, IOException {
+		response.sendRedirect(url);
+	}
+
 }
