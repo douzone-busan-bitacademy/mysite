4bdb84161a2cf73866d648eed6b882690a83446f
douzone-busan-bitacademy <douzone.busan.bitacademy@gmail.com>
2021-05-31 13:01:50 +0900

    board #2

--- docs/user.sql
+++ docs/user.sql
@@ -8,3 +8,9 @@ values(null, '관리자', 'admin@mysite.com', '1234', 'male');
 
 -- user list(select)
 select * from user;
+
+-- login(select)
+select no, name
+  from user
+ where email='kickscar@gmail.com'
+   and password='1234';
